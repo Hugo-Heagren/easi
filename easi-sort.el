4d26634117815511f6d3b58a29f43bc991f3d430
Hugo Heagren <hugo@heagren.com>
2023-02-19 13:40:32 +0000

    new module: easi-sort

---
+++ easi-sort.el
@@ -0,0 +1,28 @@
+;;; easi-sort.el --- Sorting and mixing functionality in EASI  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Hugo Heagren
+
+;; Author: Hugo Heagren <hugo@undertown>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module implements customisable sorting of results from more
+;; than one source in EASI.
+
+;;; Code:
+
+(provide 'easi-sort)
+;;; easi-sort.el ends here
