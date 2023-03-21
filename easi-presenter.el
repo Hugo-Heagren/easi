0f1a8f54e8f5a3076c1de3b7c463af398a378ca1
Hugo Heagren <hugo@heagren.com>
2023-03-21 21:13:52 +0000

    new presenter: tabulated-list-results-list
    Define a presenter which presents results in tabulated-list-mode.

--- easi-presenter.el
+++ easi-presenter.el
@@ -25,6 +25,7 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'easi-result)
 
 ;;;; Customizables
 
@@ -79,6 +80,125 @@ engines.")
   ;; ACTION arg to pass to `display-buffer'
   display-action
   hook)
+
+;;;; Presenters
+;;;;; tabulated list results presenter
+
+(defgroup easi-results-list nil
+  "Group for EASI results presenter based on `tabulated-list-mode'."
+  :group 'easi)
+
+(defcustom easi-results-list-fields nil
+  "List of fields for `easi-results-list-presenter' to display.
+
+Each element is of the form (FIELD WIDTH SORT).
+
+FIELD is a string, the name of the field from each result to
+include.
+
+WIDTH is an integer, the maximum width to print.
+
+If SORT is non-nil, this field can be sorted. It is either
+t (indicating to sort alphabetically on strings), or a predicate."
+  :group 'easi-results-list
+  :type '(repeat (list
+		  (string :tag "Field")
+		  (integer :tag "Width")
+		  (choice :tag "Sort"
+		   (const :tag "Sort on strings" t)
+		   function))))
+
+(defun easi-results-list--convert-field-list (list)
+  "Make easi-results-list field tabulated-list compatible.
+
+If the first element of LIST is a list, return a list of the
+first non-list item in LIST, then the cdr of LIST. (i.e. for
+'(((foo bar)) baz qux)) return (foo baz qux). Otherwise return LIST.
+
+If this function is mapped over `easi-results-list-fields', the
+result is an acceptable value for `tabulated-list-format'."
+  (if (listp (car list))
+      (cons (car (flatten-list list)) (cdr list))
+    list))
+
+(define-derived-mode easi-results-list-mode tabulated-list-mode "easi-results-list-mode"
+  "Major mode for viewing EASI results in a tabulated list. Derived
+from `tabulated-list-mode'."
+  :group 'easi-results-list
+  ;; NOTE This isn't really needed, because this mode is always (?)
+  ;; called from a freshly created buffer. But calling it first is one
+  ;; of the Emacs major mode conventions.
+  (kill-all-local-variables)
+  (setq major-mode 'easi-results-list-mode)
+  (setq tabulated-list-format
+	(seq--into-vector
+	 (mapcar #'easi-results-list--convert-field-list
+		 easi-results-list-fields))))
+
+(defun easi-results-list-mode-next (&optional arg)
+  "Move to next entry in results list.
+
+If prefix ARG is specified, move ARG entries forward."
+  (interactive "P")
+  (forward-line arg)
+  (easi--update-result))
+
+(defun easi-results-list-mode-previous (&optional arg)
+  "Move to previous entry in results list.
+
+If prefix ARG is specified, move ARG entries backward."
+  (interactive "P")
+  (forward-line (- (or arg 1)))
+  (easi--update-result))
+
+(let ((map easi-results-list-mode-map))
+  (keymap-set map "n" #'easi-results-list-mode-next)
+  (keymap-set map "p" #'easi-results-list-mode-previous))
+
+(defun easi-results-list--results-to-tabulated-list (results)
+  "Convert a list of EASI results for `tabulated-list-mode'.
+
+RESULTS should be a list of results EASI will recognise. Return a
+list of entries which `tabulated-list-mode' will recognise and be
+able to print."
+  (cl-loop for res in results
+	   when res
+	   collect
+	   ;; Using the result object as the id allows us to use
+	   ;; `tabulated-list-get-id' to get the current result
+	   `(,res
+	     ,(seq--into-vector
+	       (mapcar (lambda (field) (or (easi-result-get-field (car field) res)
+				      ;; Use an empty string, for
+				      ;; cases without a field value
+				      ""))
+		       easi-results-list-fields)))))
+
+(defun easi-results-list--mode-setup (results _buffer)
+  "Initialisation function for `easi-results-list-mode'.
+
+- Turn on `easi-results-list-mode' in the current buffer
+- convert RESULTS using
+  `easi-results-list--results-to-tabulated-list', and
+  `tabulated-list-entries' to the return value
+- evaluate `tabulated-list-init-header'"
+  (easi-results-list-mode)
+  (setq tabulated-list-entries
+	(easi-results-list--results-to-tabulated-list results))
+  (tabulated-list-init-header))
+
+(defun easi-results-list--print (_results _buffer)
+  "Call `tabulated-list-print' with non-nil REMEMBER-POS."
+  (tabulated-list-print 'remember-pos))
+
+(defvar easi-results-list-presenter
+  (easi-results-presenter-create
+   :name "Tabulated list presenter"
+   :key "l"
+   :before '(easi-results-list--mode-setup)
+   :result-printer '(easi-results-list--print)
+   :current-result-getter #'tabulated-list-get-id)
+  "Results presenter based on `tabulated-list-mode'.")
 
 
 (provide 'easi-presenter)
