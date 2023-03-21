c618d87670334663b09e16181865547018c23e5c
Hugo Heagren <hugo@heagren.com>
2023-03-21 21:48:02 +0000

    new presenter: tabulated-list-result-list
    Define a presenter which presents a result in tabulated-list-mode.

--- easi-presenter.el
+++ easi-presenter.el
@@ -200,6 +200,103 @@ able to print."
    :current-result-getter #'tabulated-list-get-id)
   "Results presenter based on `tabulated-list-mode'.")
 
+;;;;; tabulated list result presenter
+
+(defgroup easi-result-list nil
+  "Group for EASI result presenter based on `tabulated-list-mode'."
+  :group 'easi)
+
+(defcustom easi-result-list-exlude-fields nil
+  "List of fields to exlude.
+
+At the moment this is just a list of strings."
+  :group 'easi-result-list
+  :type '(repeat string))
+
+;; TODO Enforce major mode conventions (see info node (elisp)Major
+;; Mode Conventions)
+(define-derived-mode easi-result-list-mode tabulated-list-mode "easi-result-list-mode"
+  "Major mode for viewing an EASI result in a tabulated list,
+presenting columns of field names and values. Derived from
+‘tabulated-list-mode’."
+  :group 'easi-result-list
+  ;; NOTE This isn't really needed, because this mode is always (?)
+  ;; called from a freshly created buffer. But calling it first is one
+  ;; of the Emacs major mode conventions.
+  (kill-all-local-variables)
+  ;; Setup format
+  (setq tabulated-list-format [("Field" 30 t)
+			       ("Value" 30 t)]))
+
+(defun easi-result-list--result-to-tabulated-list (result)
+  "Convert an EASI result for `tabulated-list-mode'.
+
+Uses `easi-result-list-fields' to get a list of fields. Return a
+list, suitable as a value for `tabulated-list-entries', with each
+element having the field string as it's ID and DESC1, and the
+value of that field as DESC2."
+  (cl-loop for field in (easi-result-list-fields result)
+	   unless (cl-member
+		   field easi-result-list-exlude-fields :test #'string=)
+	   collect
+	   `(,field
+	     [,field
+	      (,(let ((val (easi-result-get-field field result)))
+		  (cond
+		   ((stringp val) val)
+		   ((prin1-to-string val))
+		   (t ""))))])
+	   ;; End unless
+	   end))
+
+;; (defun easi-result-list--mode-setup (result _buffer)
+;;   "Initialisation function for `easi-result-list-mode'.
+
+;; - run `easi-result-list-mode'
+;; - run `tabulated-list-init-header'"
+;;   (easi-result-list-mode)
+;;   (setq tabulated-list-entries
+;; 	(easi-result-list--result-to-tabulated-list result))
+;;   (tabulated-list-init-header))
+
+;; (defun easi-result-list--print (result _buffer)
+;;   ""
+;;   (tabulated-list-print 'remember-pos))
+
+(defun easi-result-list--mode-setup (result _buffer)
+  "Initialisation function for `easi-result-list-mode'.
+
+- run `easi-result-list-mode'
+- run `tabulated-list-init-header'"
+  (easi-result-list-mode)
+  (tabulated-list-init-header))
+
+(defun easi-result-list--print (result _buffer)
+  "Set entries and print.
+
+Set `tabulated-list-entries' on the basis of RESULT, then call
+`tabulated-list-print'."
+  ;; NOTE Unlike `easi-results-list--print', the result presenter has
+  ;; to set `tabulated-list-entries' again each time it prints (rather
+  ;; than in the setup). This is because the setup function is only
+  ;; run once, when the buffers are initially setup, but the result
+  ;; buffer needs to display different information each time a new
+  ;; result is selected.
+  (setq tabulated-list-entries
+	(easi-result-list--result-to-tabulated-list result))
+  (tabulated-list-print 'remember-pos))
+
+(defvar easi-result-list-presenter
+  (easi-result-presenter-create
+   :name "Tabulated list result presenter"
+   :key "t"
+   :before '(easi-result-list--mode-setup)
+   :field-printer '(easi-result-list--print)
+   :current-field-getter #'tabulated-list-get-id
+   :display-action '(display-buffer-at-bottom
+		     . ((inhibit-same-window . t) ;; Don't reuse same window
+			(window-height . 20)))) ;; 20 lines high
+  "Result presenter based on `tabulated-list-mode'.")
 
 (provide 'easi-presenter)
 ;;; easi-presenter.el ends here
