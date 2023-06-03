fa9d6256a7411febd528796606869a393aa2bcac
Hugo Heagren <hugo@heagren.com>
2023-06-03 23:24:25 +0100

    easi-result-buffer: remove
    Unnecessary

--- easi.el
+++ easi.el
@@ -199,9 +199,6 @@ If a session is created, it is added to `easi-session-list'."
 
 ;;;; Results user interface
 
-(defvar-local easi-result-buffer nil
-  "Buffer displaying current EASI result.")
-
 (defun easi--buffer-from-default (default session)
   "Get an appropriate buffer given DEFAULT.
 
